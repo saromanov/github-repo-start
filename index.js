784eacf7835d68abe48e16aab93218cc49dcc98c
Sergey Romanov <xxsmotur@gmail.com>
2015-06-10 00:08:09 +0500

    Change to sorted version

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ var repo = require('github-repositories');
 var moment = require('moment');
 var chalk = require('chalk');
 
-module.exports = function(title) {
+module.exports = function(title, opts) {
+    opts = opts || {};
     repo(title, function(error, response) {
+        repos = [];
         response.forEach(function(repo) {
-            console.log(chalk.bold.blue(repo.name), chalk.green(moment(repo.created_at).calendar()));
+            repos.push([repo.name, repo.created_at]);
+        });
+
+        result = repos.sort(function(item1, item2){
+            if (moment(item1[1]).isBefore(item2[1])) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+
+        result.forEach(function(repo){
+            console.log(chalk.bold.green(repo[0]), chalk.bold.blue(moment(repo[1]).calendar()));
         });
     });
 };
